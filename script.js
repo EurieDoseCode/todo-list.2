7ae7750d92ebba3a828fa5f33e808985904ef362
Yasu-Kun <katsuroyasu@gmail.com>
2022-11-27 23:55:39 +1100

    added extra background, modified more css, and swap the date/time around

--- script.js
+++ script.js
@@ -21,6 +21,6 @@ function printDate(){
 
     const dateList = ["", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11st", "12nd", "13rd", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "31st"]
 
-    todayDate.innerHTML = time + " ✩ " + dateList[date] + " " + monthList[month] + ", " + year;
+    todayDate.innerHTML = dateList[date] + " " + monthList[month] + ", " + year + " ✩ " + time;
 }
 setInterval(printDate, 1000);
